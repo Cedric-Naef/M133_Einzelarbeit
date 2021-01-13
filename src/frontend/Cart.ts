f56d88da236e8bdf6f163dcb8b1cb7f49b171a16
cedric <Cedric_Naef@sluz.ch>
2021-01-13 09:30:10 +0100

    Adding List and Link to Overview

--- src/frontend/Cart.ts
+++ src/frontend/Cart.ts
@@ -4,9 +4,9 @@ import { Product } from "../common/types";
 
 export async function loadDetail() {
     const productId = new URLSearchParams(window.location.search).get("productId"); 
-    const response = await fetch(`/api/persons/${productId}`);
-    const person: Product = await response.json();
+    const response = await fetch(`/api/product/${productId}`);
+    const product: Product = await response.json();
 
-    document.querySelector("h1").innerText = `${person.firstName} ${person.lastName}`;
-    document.querySelector("span").innerText = person.id;
+    document.querySelector("h1").innerText = `${product.productName} ${product.specialOffer}`;
+    document.querySelector("span").innerText = product.id;
 }
